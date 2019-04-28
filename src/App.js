3a488d9c5c10d23f26584334930a06bb0a6bfba6
ziratsu <enzo.ustariz@hotmail.fr>
2019-04-28 20:25:10 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './composants/interface/Header'
+import Liste from './composants/contact/Liste'
+import { Provider } from './context'
+import AddContact from './composants/contact/AddContact'
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Provider>
+      <div className="App">
+      <Header />
+      <div className="container">
+        <AddContact />
+        <Liste />
+      </div>
+      </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
